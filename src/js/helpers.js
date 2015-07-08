63498b2bfdc5419c4db4a4a47405fbe4bdceb0e9
mitchthorson <mthorson@usatoday.com>
2015-07-08 10:23:47 -0400

    refactor slug code to helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -3,6 +3,14 @@ function() {
     return {
         cleanName: function(string) {
             return string.trim().replace(/\s/g, "-").replace(/[\.,\/#!$%\^&\*;:{}=_`~()]/g,"").toLowerCase();
+        },
+        slugify: function(text) {
+            return text.toString().toLowerCase()
+                .replace(/\s+/g, '-')           // Replace spaces with -
+                .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                .replace(/^-+/, '')             // Trim - from start of text
+                .replace(/-+$/, '');            // Trim - from end of text
         }
     };
 });
--- src/js/models/EntryModel.js
+++ src/js/models/EntryModel.js
@@ -9,17 +9,8 @@ define([
             this.set({
                 "searchName": helpers.cleanName(this.get('Agency')),
                 "Total_untested_kits_pretty": this.get("Total_untested_kits").toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
-                "slug": this.slugify(this.get('Agency'))
+                "slug": helpers.slugify(this.get('Agency'))
             });
-        },
-        slugify: function(text) {
-            return text.toString().toLowerCase()
-                .replace(/\s+/g, '-')           // Replace spaces with -
-                .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-                .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-                .replace(/^-+/, '')             // Trim - from start of text
-                .replace(/-+$/, '');            // Trim - from end of text
         }
-
     });
 });
